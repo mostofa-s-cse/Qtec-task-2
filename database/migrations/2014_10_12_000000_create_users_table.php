538119db1c90f42ef92b3cb7c2a82d8de7002e01
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-02-11 14:49:21 +0600

    done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('types')->nullable();;
+            $table->string('types')->default(2);
             $table->rememberToken();
             $table->timestamps();
         });
